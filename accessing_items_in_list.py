98e2851018612e54bd2ecb5bcc6d5b7480aad082
Eduardo Paixao Viccari <eduardo.viccari@outlook.com>
2021-10-26 00:47:56 -0300

    initial commit

---
+++ accessing_items_in_list.py
@@ -0,0 +1,8 @@
+letters = ["a", "b", "c", "d"]
+print(letters[0])
+
+numbers = list(range(20))
+print(numbers[-1])
+print(numbers[0:3])
+print(numbers[::2])
+print(numbers[::-1])
---
+++ lists.py
@@ -0,0 +1,7 @@
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(20))
+chars = list("Hello Henrique")
+print(len(chars))
